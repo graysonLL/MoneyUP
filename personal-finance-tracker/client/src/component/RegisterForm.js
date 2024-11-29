5f61060c9f9e76cd712aad96a577e611d241fdde
Matt-ethan <matthewethan6@gmail.com>
2024-11-29 22:05:08 +0800

    changed the input of the number area for the register page from number to text

--- personal-finance-tracker/client/src/component/RegisterForm.js
+++ personal-finance-tracker/client/src/component/RegisterForm.js
@@ -4,7 +4,7 @@ import "../styles/registerform.css";
 const RegisterForm = ({ onSubmit }) => {
   return (
     <form className="form" onSubmit={onSubmit}>
-        <input type="number" placeholder="Number" className="input" required />
+        <input type="text" placeholder="Number" className="input" required />
       <input type="email" placeholder="Email" className="input" required />
       <input type="password" placeholder="Password" className="input" required />
       <input type="password" placeholder="Confirm Password" className="input" required />
