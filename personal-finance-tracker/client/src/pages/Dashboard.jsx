84535b31ee344e51f85c0b9673705142a46b6290
graysonLL <liam.m.a.jones@gmail.com>
2024-12-16 17:22:19 +0800

    removed low balance warning on dashboard

--- personal-finance-tracker/client/src/pages/Dashboard.jsx
+++ personal-finance-tracker/client/src/pages/Dashboard.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import LowBalanceWarning from "../components/dashboard/LowBalanceWarning";
 import FinancialSummary from "../components/dashboard/FinancialSummary";
 import RecentUpdates from "../components/dashboard/RecentUpdates";
 import "../styles/Dashboard.css";
 
 const Dashboard = ({ isSidebarOpen }) => {
-  const currentBalance = 900;
-
   return (
     <div className={`dashboard ${isSidebarOpen ? "" : "sidebar-closed"}`}>
       <div className="dashboard-content">
-        <LowBalanceWarning currentBalance={currentBalance} />
         <FinancialSummary />
         <RecentUpdates />
       </div>
